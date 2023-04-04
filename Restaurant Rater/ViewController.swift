2de72c7796ea90bfa758814accfa9c50f232f8f7
Jalen Lewis <jaylewis0618@gmail.com>
2023-04-03 23:17:24 -0500

    Initial Commit

---
+++ Restaurant Rater/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant Rater
+//
+//  Created by user238354 on 4/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
