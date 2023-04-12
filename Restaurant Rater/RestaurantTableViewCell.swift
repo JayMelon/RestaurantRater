ba35d28ecf080db9a79293d9b759a73cc72e44bc
Jalen Lewis <jaylewis0618@gmail.com>
2023-04-11 23:34:53 -0500

    Help

--- Restaurant Rater/RestaurantTableViewCell.swift
+++ Restaurant Rater/RestaurantTableViewCell.swift
@@ -10,18 +10,18 @@ import UIKit
 class RestaurantTableViewCell: UITableViewCell {
     
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var addressLabel: UILabel!
-    @IBOutlet weak var dishesLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
     
     
     func configure(with restaurant: Restaurant){
         nameLabel.text = restaurant.name
-        addressLabel.text = restaurant.address
+        nameLabel.text = restaurant.address
         
         
         let dishes = restaurant.dishes?.allObjects as? [Dish] ?? []
         let dishInfo = dishes.map {"\($0.dish ?? "") (\($0.type ?? "")): \($0.rating    )"}
-        dishesLabel.text = dishInfo.joined(separator: "\n")
+        ratingLabel.text = dishInfo.joined(separator: "\n")
     }
     
     
